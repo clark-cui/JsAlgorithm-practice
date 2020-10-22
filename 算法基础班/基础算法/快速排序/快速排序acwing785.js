5c882ff832adf100b889701bec6629266db6fcca
clark-cui <cuirongchuan@163.com>
2020-10-22 11:11:39 +0800

    add 从大到小

--- 算法基础班/基础算法/快速排序/快速排序acwing785.js
+++ 算法基础班/基础算法/快速排序/快速排序acwing785.js
@@ -48,8 +48,8 @@ function quick_sort(q, l, r) {
     //l+r>>1 是位操作符，与c++相同，表示(l+r)/2取整数
     i = l - 1, j = r + 1, x = q[l + r >> 1];
     while (i < j) {
-        do i++; while (q[i] < x);
-        do j--; while (q[j] > x);
+        do i++; while (q[i] < x); //如果从大到小，这里<变>即可
+        do j--; while (q[j] > x);//如果从大到小，这里>变<即可
         if (i < j) {
             // swap(q[i], q[j]); 交换q[i]与q[j]，js没有swap
             let nB = q[i];
